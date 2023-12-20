b7e8afd3b219bb6930c67f59cbc243113501c91a
Rafael Urben <rafael.urben@ict-campus.net>
2023-12-20 16:08:28 +0100

    Added contact form

---
+++ assets/js/contactform.js
@@ -0,0 +1,8 @@
+function validateContactForm() {
+    document.querySelector("#contact-form").classList.add("validated");
+}
+
+
+function submitContactForm() {
+    // ToDo: open email client with entered message and respond-to prefilled
+}
