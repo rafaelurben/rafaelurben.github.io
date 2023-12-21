73fad711720a58472ae70f597d92f3b24b2a38ca
Rafael Urben <rafael.urben@ict-campus.net>
2023-12-21 09:15:06 +0100

    Kontaktformular aktualisiert und ergänzt

--- assets/js/contactform.js
+++ assets/js/contactform.js
@@ -4,5 +4,16 @@ function validateContactForm() {
 
 
 function submitContactForm() {
-    // ToDo: open email client with entered message and respond-to prefilled
+    let subject = document.querySelector("#form-contact-subject").value;
+    let message = document.querySelector("#form-contact-message").value;
+
+    // Because this is a static website, open the email client with the above message prefilled.
+
+    let url = `mailto:dev@rafaelurben.ch?`;
+    url += new URLSearchParams({
+        subject: "Kontaktanfrage: " + subject,
+        body: message
+    }).toString()
+
+    window.open(url);
 }
