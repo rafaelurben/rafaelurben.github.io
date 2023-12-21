4a2160de773b0c0bb24b9cff9f934b40872533f8
Rafael Urben <rafael.urben@ict-campus.net>
2023-12-21 14:29:31 +0100

    Added link to impressum

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,5 @@ function toggleNavigation() {
 /* Onload */
 
 window.addEventListener('load', () => {
-    document.querySelector("nav a[href*='" + location.pathname + "']").classList.add("current");
+    document.querySelector("a[href*='" + location.pathname + "']").classList.add("current");
 })
